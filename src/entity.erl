c6fe1fc513e639bdbd2f97ea4635ff7f40b10cb8
Joakim Ejenstam <Joakim@Joakims-MacBook-Pro.local>
2013-01-10 20:49:11 +0100

    Små rosa hästar!!!

--- src/entity.erl
+++ src/entity.erl
@@ -4,15 +4,16 @@
 -behaviour(gen_server).
 
 
-start_cell() ->
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_cell(Neighbours) ->
+	gen_server:start_link(?MODULE, [Neighbours], []).
 
 %% ====================================================================
 %% callback functions
 %% ====================================================================
 
-init([]) ->
-	{ok, [[],[]]}.
+init(Neighbours) ->
+	Type = empty,
+	{ok,[Neighbours, Type]}.
 
 handle_call(_, _From, State) ->
 	{reply, ok, State}.
@@ -21,6 +22,22 @@ handle_cast(_, State) ->
 	{noreply, State}.
 
 handle_info(Info, State) ->
+	case Info of
+		{spawn_plant, From} ->
+			From ! gen_server:call(?MODULE, spawn_plant);
+		{spawn_herbivore, From} ->
+			From ! gen_server:call(?MODULE, spawn_herbivore);
+		{spawn_carnicore, From} ->
+			From ! gen_server:call(?MODULE, spawn_carnicore);
+		{move_herbivore, From} ->
+			From ! gen_server:call(?MODULE, move_herbivore);
+		{move_carnivore, From} ->
+			From ! gen_server:call(?MODULE, move_carnivore);
+		{eat_grass, From} ->
+			From ! gen_server:call(?MODULE, eat_grass);
+		{eat_herbivore, From} ->
+			From ! gen_server:call(?MODULE, eat_herbivore);
+	end
 	{noreply, State}.
 
 terminate(_Reason, _State) ->
