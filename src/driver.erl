4ffc71b0aba50abc13a8c376714df186f3b5058d
Joakim Ejenstam <joakim.ejenstam@gmail.com>
2013-01-17 18:33:28 +0100

    Jocke har fixat ska ni se!

--- src/driver.erl
+++ src/driver.erl
@@ -21,7 +21,12 @@ parse_binary_inner(<<Byte:8, Rest/binary>>, Ack, X, Y) ->
  
 parse_binary_outer(<<>>, Ack, _, _) -> Ack;
 parse_binary_outer(Board, Ack, Y, W) ->
-        Board_width = 8*(W+1),
+        case binary:referenced_byte_size(Board) of
+                120 -> 
+                        Board_width=120 div W;
+                _ -> 
+                        Board_width = 8*(W+1)
+        end,
         <<Line:Board_width, Rest/binary>> = Board,
         parse_binary_outer(Rest, parse_binary_inner(binary:encode_unsigned(Line), Ack, 0, Y), Y+1, W).
  
@@ -56,7 +61,7 @@ loop(P, E, W, H) ->
                         driver:loop(P,E,W,H);
                 {stop} ->
                         exit(whereis(clock), kill),
-                        lists:map(fun(X) -> exit(X, kill) end, P),
+                        lists:map(fun(X) -> gen_server:cast(X, stop) end, P),
                         driver:loop(P,E,W,H);
                 _ ->
                         io:format("Driver: Undefined message~n",[])
